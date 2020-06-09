2a318eb9d519ed29b304a31a521858ffb5de06f0
arsalanpeerzada <peerzada_1994@live.com>
2020-06-09 21:33:56 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:newchatapp/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primaryColor : Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
